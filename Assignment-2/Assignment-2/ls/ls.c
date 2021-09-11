53c29a2c76793bd981847c1813bdfec31f9223e8
Miryala Narayana Reddy <narayana@git.com>
2021-09-11 08:35:21 +0530

    ls works with mutiple  -l , -a ,directory names

--- Assignment-2/Assignment-2/ls/ls.c
+++ Assignment-2/Assignment-2/ls/ls.c
@@ -3,17 +3,126 @@
 #include <pwd.h> // getpwuid
 #include <grp.h> // getgrgid
 #include <time.h>
+#include <string.h>
 
-void print_name(char *name, bool details)
+void ls(char *args)
 {
-    if (!details)
+    char cwd[MAX_PATH_LEN];
+    getcwd(cwd, MAX_PATH_LEN);
+
+    char dir_paths[10][MAX_PATH_LEN];
+    int count = 0;
+    bool l = false, a = false;
+
+    // tokenize
+
+    char *token;
+    char *strptr;
+
+    token = strtok_r(args, " ", &strptr);
+
+    while (token != NULL)
     {
-        printf("%s\n", name);
+        if (AreSame(token, "-l"))
+        {
+            l = true;
+        }
+        else if (AreSame(token, "-al") || AreSame(token, "-la"))
+        {
+            l = a = true;
+        }
+        else if (AreSame(token, "-a"))
+        {
+            a = true;
+        }
+        else
+        {
+            strcpy(dir_paths[count], token);
+            count++;
+        }
+
+        token = strtok_r(NULL, " ", &strptr);
+    }
+    // get flags
+    char flags[4];
+    if (l & a)
+    {
+        strcpy(flags, "-al");
+    }
+    else if (l)
+    {
+        strcpy(flags, "-l");
+    }
+    else if (a)
+    {
+        strcpy(flags, "-a");
     }
     else
     {
-        list_details(name);
-        printf("\n");
+        strcpy(flags, "");
+    }
+
+    // for each directory
+    if (count == 0)
+    {
+        process_ls(cwd, flags); // call for processing
+    }
+    else
+    {
+        char ls_wd[MAX_PATH_LEN]; //ls working directory
+        for (int i = 0; i < count; i++)
+        {
+            // printf("%s\n",dir_paths[i]);
+            bool failed = chdir(dir_paths[i]); //returns 1 if fails.
+            if (!failed)
+            {                                // we changed into that directory now do ls as if we in that directory (of course we are in it)
+                getcwd(ls_wd, MAX_PATH_LEN); // get working directory path after changing directory
+                printf("%s:\n", ls_wd);
+                process_ls(ls_wd, flags); // call for processing
+                chdir(cwd);               // change back to original directory.
+                printf("\n");
+            }
+            else
+            {
+                if (AreSame(dir_paths[i], "~"))
+                {
+                    chdir(USER_HOME_PATH);
+                    printf("%s:\n", ls_wd);
+                    process_ls(USER_HOME_PATH, flags); // call for processing
+                    chdir(cwd);                        // change back to original directory.
+                    printf("\n");
+                }
+                else
+                {
+                    printf("%s\n: No such directory\n", dir_paths[i]);
+                }
+            }
+        }
+    }
+}
+
+void process_ls(char *cwd, char *flags)
+{
+    if (AreSame(flags, ""))
+    {
+        list_names(cwd, false, false); // do not list hidden files
+    }
+    else if (AreSame(flags, "-a"))
+    {
+        list_names(cwd, true, false); // list all
+    }
+    else if (AreSame(flags, "-l"))
+    {
+        list_names(cwd, false, true);
+    }
+    else if (AreSame(flags, "-al"))
+    {
+        list_names(cwd, true, true);
+    }
+    else
+    {
+        printf("%s\n No such directory", cwd);
+        // never happens as far as I see ... but just added it for completeness and neatness :)
     }
 }
 
@@ -30,18 +139,36 @@ void list_names(char *path, bool include_hidden_files, bool details)
         {
             if (curr_dir->d_type == DT_DIR)
             {
-                Color_On(__BLUE, BOLD);
-                print_name(curr_dir->d_name, details);
-                Color_Off();
+                print_name(curr_dir->d_name, details, true);
             }
             else
             {
-                Bold(true);
-                print_name(curr_dir->d_name, details);
-                Bold(false);
+                print_name(curr_dir->d_name, details, false);
             }
         }
         curr_dir = readdir(dir);
+    }
+}
+
+void print_name(char *name, bool details, bool isdir)
+{
+    if (details)
+    {
+        list_details(name); // list details i.r., in long format
+    }
+
+    // print name in color depending on its type (directory or file)
+    if (isdir)
+    {
+        Color_On(__BLUE, BOLD);
+        printf("%s\n", name);
+        Color_Off();
+    }
+    else
+    {
+        Bold(true);
+        printf("%s\n", name);
+        Bold(false);
     }
 }
 
@@ -75,54 +202,6 @@ void list_details(char *name)
     struct timespec last_modify_time = file_stats.st_mtim;
     struct tm *lmdate = localtime(&(last_modify_time.tv_sec));
     printf("%s %2d %2d:%2d ", month_name(lmdate->tm_mon), lmdate->tm_mday, lmdate->tm_hour, lmdate->tm_min);
-
-    printf("%s ", name);
-}
-
-void ls(char *args)
-{
-    char cwd[MAX_PATH_LEN];
-    getcwd(cwd, MAX_PATH_LEN);
-
-    if (AreSame(args, ""))
-    {
-        list_names(cwd, false, false); // do not list hidden files
-    }
-    else if (AreSame(args, "-a"))
-    {
-        list_names(cwd, true, false); // list all
-    }
-    else if (AreSame(args, "."))
-    {
-        list_names(cwd, false, false);
-    }
-    else if (AreSame(args, ".."))
-    {
-        chdir("../");
-        char temp[MAX_PATH_LEN];
-        getcwd(temp, MAX_PATH_LEN);
-        list_names(temp, false, false);
-        chdir(cwd);
-    }
-    else if (AreSame(args, "~"))
-    {
-        chdir(USER_HOME_PATH);
-        char temp[MAX_PATH_LEN];
-        getcwd(temp, MAX_PATH_LEN);
-        list_names(temp, false, false);
-        chdir(cwd);
-    }
-    else if (AreSame(args, "-l"))
-    {
-        list_names(cwd, false, true);
-    }
-    else if (AreSame(args, "-al"))
-    {
-        list_names(cwd, true, true);
-    }
-    else
-    {
-    }
 }
 
 char *month_name(int month)
--- Assignment-2/Assignment-2/ls/ls.h
+++ Assignment-2/Assignment-2/ls/ls.h
@@ -5,8 +5,9 @@
 #include <dirent.h>
 
 void ls(char *args);
+void process_ls(char *cwd, char *flags);
 void list_names(char *path, bool include_hidden_files, bool details);
-void print_name(char *name, bool details);
+void print_name(char *name, bool details, bool isdir);
 void list_details(char *name);
 char *month_name(int month);
 
