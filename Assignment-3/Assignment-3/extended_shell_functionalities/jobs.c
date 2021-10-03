53a3cf303c8b5f59d6f6363d428daf24bad54c7b
narayana <narayana@git.com>
2021-10-03 19:23:10 +0530

    jobs working

--- Assignment-3/Assignment-3/extended_shell_functionalities/jobs.c
+++ Assignment-3/Assignment-3/extended_shell_functionalities/jobs.c
@@ -1,5 +1,7 @@
 #include "jobs.h"
 #include <fcntl.h>
+#include <wait.h>
+
 void push_into_jobs(char *p, int pid)
 {
     strcpy(jobs[num_jobs].name, p);
@@ -50,24 +52,48 @@ char *expand_status(char ch)
 void list_jobs(char *args)
 {
     char st;
+    FILE *fp = fopen("temp_XXXX.txt", "w");
+    fclose(fp);
+    fp = fopen("temp_XXXX.txt", "a");
+
     if (AreSame(args, ""))
         for (int i = 0; i < num_jobs; i++)
         {
             st = pstatus(jobs[i].pid);
-            printf("[%d] %s %s [%d]\n", i + 1, expand_status(st), jobs[i].name, jobs[i].pid);
+            fprintf(fp, "[%d] %s %s [ %d ]\n", i + 1, expand_status(st), jobs[i].name, jobs[i].pid);
         }
     else if (AreSame(args, "-r"))
         for (int i = 0; i < num_jobs; i++)
         {
             st = pstatus(jobs[i].pid);
             if (st == 'R')
-                printf("[%d] %s %s [%d]\n", i + 1, expand_status(st), jobs[i].name, jobs[i].pid);
+                fprintf(fp, "[%d] %s %s [ %d ]\n", i + 1, expand_status(st), jobs[i].name, jobs[i].pid);
         }
     else if (AreSame(args, "-s"))
         for (int i = 0; i < num_jobs; i++)
         {
             st = pstatus(jobs[i].pid);
-            if (st == 'S')
-                printf("[%d] %s %s [%d]\n", i + 1, expand_status(st), jobs[i].name, jobs[i].pid);
+            if (st == 'T')
+                fprintf(fp, "[%d] %s %s [ %d ]\n", i + 1, expand_status(st), jobs[i].name, jobs[i].pid);
         }
+    fclose(fp);
+    int s;
+    int pid = fork();
+
+    if (pid == 0)
+    {
+        // dup2(STDOUT_FILENO,STDOUT_FILENO);
+        execl("/bin/sort", "sort", "-k3,3","-k5,5n" ,"temp_XXXX.txt", NULL);
+        exit(0);
+    }
+    else if (pid < 0)
+    {
+        perror("Error forking for sort");
+    }
+    else
+    {
+        waitpid(pid, &s, 0);
+    }
+
+    remove("temp_XXXX.txt");
 }
